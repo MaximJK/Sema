acd4df59987ef63d83cd4133e2ec257889464256
your name here <maxjacobkoiwai@gmail.com>
2019-10-17 17:41:03 -0400

    poem onclick

--- src/APIreqs.js
+++ src/APIreqs.js
@@ -1,9 +1,49 @@
-const titlesFetch = "http://poetrydb.org/title"
-let request = new XMLHttpRequest()
+const fetch = require('node-fetch');
 
-const response = fetch(titlesFetch).then(data => data.json()).then(data => data.map(data => data.titles))
-let title = response[Math.floor(Math.random() * response.length)];
-const poem = json.parse(fetch(`http://poetrydb.org/title/${title}`));
-const title = poem.first.title
-const author = poem.first.author
-const body = poem.first.lines
+export function getPoem() {
+    const fetchTitles = fetch("http://poetrydb.org/title")
+    const authorDiv = document.getElementById('authorDiv');
+    const poemH = document.getElementById('poemH');
+    const app = document.getElementById('app');
+    let ul = document.getElementById('poemUl');
+    let poemObj
+    let poemName;
+    fetchTitles.then(response => {
+        return response.json();
+    }).then(titlesArr => {
+        
+        const titles = titlesArr.titles
+        poemName = titles[Math.floor(Math.random() * titles.length)]
+        fetch(`http://poetrydb.org/title/${poemName}`).then(response => {
+            
+            return response.json();
+        }).then(poem => {
+            
+            poemObj = { title: poem[0].title, author: poem[0].author, lines: poem[0].lines }
+            poemObj.lines.forEach(line => {
+                let li = document.createElement("li")
+                li.textContent = line
+                ul.appendChild(li)
+            });
+            
+            let h = document.createTextNode(poemObj.title)
+            poemH.appendChild(h)
+
+            let a = document.createTextNode(poemObj.author)
+            authorDiv.appendChild(a)
+
+            app.appendChild(ul)
+            debugger
+            const lineClick = function(){
+                debugger
+                let x = event.target
+                console.log(x)
+            }
+            let lis = document.querySelectorAll('li')
+            lis.forEach(li => {
+                debugger
+                li.addEventListener("click", lineClick);
+        })
+            retrieved = true
+    })
+    })}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './styles/index.scss';
+import {
+    getPoem
+} from './APIreqs'
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('app').innerText = "Hello World!";
-});
+document.addEventListener('DOMContentLoaded', () => {
+    let retrieved = false
+
+    if (retrieved === false) {
+        debugger
+        getPoem();
+        debugger
+        retrieved = true
+    }
+
+    
+    
+    
+})
+
