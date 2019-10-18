fa1dbe4b4d3cfe3ee48c6355e65478c5192537e4
your name here <maxjacobkoiwai@gmail.com>
2019-10-18 10:19:00 -0400

    proddeploy3

--- src/APIreqs.js
+++ src/APIreqs.js
@@ -13,37 +13,38 @@ export const getPoem = async() => {
     let poemName = titles[Math.floor(Math.random() * titles.length)]
     const fetchPoem = await fetch(`http://poetrydb.org/title/${poemName}`)
     const poem = await fetchPoem.json()
-    return const poemObj = { title: poem[0].title, author: poem[0].author, lines: poem[0].lines }
-            poemObj.lines.forEach(line => {
-                let li = document.createElement("li")
-                li.textContent = line
-                ul.appendChild(li)
-            });
+    return { title: poem[0].title, author: poem[0].author, lines: poem[0].lines }
+    //         poemObj.lines.forEach(line => {
+    //             let li = document.createElement("li")
+    //             li.textContent = line
+    //             ul.appendChild(li)
+    //         });
             
-            let h = document.createTextNode(poemObj.title)
-            poemH.appendChild(h)
+    //         let h = document.createTextNode(poemObj.title)
+    //         poemH.appendChild(h)
 
-            let a = document.createTextNode(poemObj.author)
-            authorDiv.appendChild(a)
+    //         let a = document.createTextNode(poemObj.author)
+    //         authorDiv.appendChild(a)
 
-            app.appendChild(ul)
-            debugger
-            const lineClick = function(){
-                debugger
-                let x = event.target
-                console.log(x)
-            }
-            let lis = document.querySelectorAll('li')
-            lis.forEach(li => {
-                debugger
-                li.addEventListener("click", lineClick);
+    //         app.appendChild(ul)
+    //         debugger
+    //         const lineClick = function(){
+    //             debugger
+    //             let x = event.target
+    //             console.log(x)
+    //         }
+    //         let lis = document.querySelectorAll('li')
+    //         lis.forEach(li => {
+    //             debugger
+    //             li.addEventListener("click", lineClick);
 
                 
-        })
+    //     })
 
-        }).then(response => {
-            console.log(response);
-        })
-    })
-    return x
-    }
+    //     }).then(response => {
+    //         console.log(response);
+    //     })
+    // })
+    // return x
+    // }
+}
