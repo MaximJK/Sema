abab39f9185326c216907fddc006abf9449fc94f
your name here <maxjacobkoiwai@gmail.com>
2019-10-22 12:46:50 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -5,27 +5,4 @@ import {
 
 document.addEventListener('DOMContentLoaded', () => {
     getPoem();
-
-    // let ul = document.querySelectorAll('poemUl')
-    // ul.addEventListener('load', function () {
-    //     debugger
-    //     let lis = document.querySelectorAll('li')
-    //     const lineClick = function () {
-    //         debugger
-    //         let x = event.target
-    //         console.log(x)
-    //     }
-    //     lis.forEach(li => {
-    //         debugger
-    //         li.addEventListener("click", lineClick);
-// }) 
-// });
     })
-    
-
-
-    
-
-    
-// })
-
